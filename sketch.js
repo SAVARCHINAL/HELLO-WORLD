ffbfa8797f2bced238d755a9e51cfdefb8fc0fb2
SAVARCHINAL <srarchinal@dons.usfca.edu>
2022-01-27 15:26:10 -0800

    commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(1000, 800);
+
+  textSize(22);
+  textAlign(CENTER);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0,210,73);
+
+  fill(255);
+text('HELLO, WORLD',width/2, height/2);
+
+}
